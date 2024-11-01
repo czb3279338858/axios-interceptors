d64cc4cbfcbc96b7c433837879e87026f7ea1ae4
蔡朝勃 <3279338858@qq.com>
2024-11-01 14:56:02 +0800

    1

--- lib/getKey.ts
+++ lib/getKey.ts
@@ -1,5 +1,5 @@
 import { InternalAxiosRequestConfig } from "axios";
-import { cloneDeep } from "lodash-es";
+import { cloneDeep, isObject, isPlainObject } from "lodash-es";
 
 export const paramsExcludeKey: string[] = []
 
@@ -8,15 +8,18 @@ export const paramsExcludeKey: string[] = []
 function getData(config: InternalAxiosRequestConfig): string {
   const { transformRequest, data, headers } = config
   // 请求中
-  if (!config._requestId) {
-    if (Array.isArray(transformRequest)) {
-      const ret = transformRequest.reduce<any>((p, c, index) => {
+  if (!config._requestId && transformRequest) {
+
+    if (typeof transformRequest === 'function') {
+      const ret = transformRequest?.call(config, data, headers) || data
+      return JSON.stringify(ret)
+    } else {
+      // 在旧版本的axios中transformRequest可能是个{0：function}的对象
+      const ret = Object.keys(transformRequest).reduce<any>((p, key, index) => {
+        const c = transformRequest[key as unknown as number]
         if (index === 0) p = c.call(config, data, headers)
         return p
       }, null)
-      return JSON.stringify(ret)
-    } else {
-      const ret = transformRequest?.call(config, data, headers) || data
       return JSON.stringify(ret)
     }
   } else {
